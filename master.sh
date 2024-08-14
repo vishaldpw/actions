fd5f411f51d0660048bfcee172aecdaafe74f4e8
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-08-14 12:49:51 +0530

    modified master

--- master.sh
+++ master.sh
@@ -7,19 +7,21 @@
 if [ $? -eq 0 ]; then
     echo "bash1.sh executed successfully."
 
-    # Sleep for 30 seconds
-    echo "Sleeping for 30 seconds before running bash2.sh..."
-    sleep 30
+# Sleep for 30 seconds
+echo "Sleeping for 30 seconds before running bash2.sh..."
+sleep 30
 
-    # Run bash2.sh
-    ./bash2.sh
+# Run bash2.sh
+./bash2.sh
 
-    # Check if bash2.sh executed successfully
-    if [ $? -eq 0 ]; then
-        echo "bash2.sh executed successfully."
-    else
-        echo "bash2.sh failed to execute."
-    fi
+# Check if bash2.sh executed successfully
+if [ $? -eq 0 ]; then
+    echo "bash2.sh executed successfully."
+else
+    echo "bash2.sh failed to execute."
+
+fi
+
 else
     echo "bash1.sh failed to execute. Skipping bash2.sh."
 fi
