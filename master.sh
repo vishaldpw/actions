ec3fa5b5c95584146be94d301c6ae1f51bd4f8b3
Vishal Singh Chauhan <70084496+vishaldpw@users.noreply.github.com>
2024-08-19 18:09:01 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -11,9 +11,9 @@ run_script() {
     if [ $? -eq 0 ]; then
         echo "$script_name executed successfully."
 
-        # Sleep for 30 seconds before the next script
-        echo "Sleeping for 30 seconds before proceeding..."
-        sleep 30
+        # Sleep for 10 seconds before the next script
+        echo "Sleeping for 10 seconds before proceeding..."
+        sleep 10
     else
         echo "$script_name failed to execute. Skipping subsequent scripts."
         exit 1
